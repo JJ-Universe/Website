8fb5ca7d26df48a4cbe5b51da863e77591e177b0
JJ Universe <46922562+JJ-Universe@users.noreply.github.com>
2022-04-12 17:20:38 +0000

    Getting Started

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.use(express.static(`public`))
+app.use("/css", express.static(__dirname + "public/css"))
+app.use("/js", express.static(__dirname + "public/js"))
+app.use("/img", express.static(__dirname + "public/img"))
+
+app.set("views", "./views")
+app.set("view engine", "ejs")
+
+app.get("", (req, res) => {
+  res.render("home", { text: "Home Page"})
+})
+
+app.get("/home", (req, res) => {
+  res.render("home", { text: "Home Page"})
+})
+app.get("/about", (req, res) => {
+  res.render("about", { text: "About Page"})
+})
+app.get("/music/test", (req, res) => {
+  res.render("test", { text: "Test for Music Page"})
+})
+
+app.listen(port, () => console.info(`Listening on port ${port}`))
