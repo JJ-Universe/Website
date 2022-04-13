23ad91242e7fd79a1c547377b3777de20e12f8dc
JJISCOOL1020 <jermaineblount33@gmail.com>
2022-04-12 23:24:08 -0700

    Adding Music Player

--- app.js
+++ app.js
@@ -6,22 +6,26 @@ app.use(express.static(`public`))
 app.use("/css", express.static(__dirname + "public/css"))
 app.use("/js", express.static(__dirname + "public/js"))
 app.use("/img", express.static(__dirname + "public/img"))
+app.use("/music", express.static(__dirname + "public/music"))
 
 app.set("views", "./views")
 app.set("view engine", "ejs")
 
-app.get("", (req, res) => {
-  res.render("home", { text: "Home Page"})
+app.get("", (req) => {
+  res.render("home")
 })
 
-app.get("/home", (req, res) => {
-  res.render("home", { text: "Home Page"})
+app.get("/home", (req) => {
+  res.render("home")
 })
-app.get("/about", (req, res) => {
-  res.render("about", { text: "About Page"})
+app.get("/about", (req) => {
+  res.render("about")
 })
-app.get("/music/test", (req, res) => {
-  res.render("test", { text: "Test for Music Page"})
+app.get("/music/home", (req) => {
+  res.render("music/selection")
+})
+app.get("/music/player", (req) => {
+  res.render("music/player/index")
 })
 
 app.listen(port, () => console.info(`Listening on port ${port}`))
---
+++ public/js/music/player/music-list.js
@@ -0,0 +1,14 @@
+let allMusic = [
+    {
+        name: "As The World Caves In",
+        artist: "Sarah Cothran",
+        img: "",
+        src: "music/1"
+    },
+    {
+        name: "Love Is Gone (Danny Olson Remix)",
+        artist: "Slander, Dylan Matthew",
+        img: "",
+        src: "music/2"
+    }
+];
